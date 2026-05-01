3a786dd4feb00abd4fa52aa101b6e75525b99e99
Giovani Raci Paganini <gilracipaganini@gmail.com>
2026-05-01 13:42:05 -0300

    Change email label to 'Usuário' and update authorization

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -15,16 +15,16 @@ export const authOptions : NextAuthOptions = {
     CredentialsProvider ( {
       name : "credentials" ,
       credentials : {
-        email : { label : "Email" , type : "text" } ,
+        email : { label : "Usuário" , type : "text" } ,
         password : { label : "Password" , type : "password" } ,
       } ,
       async authorize ( credentials ) {
-        if ( ! credentials ?. email || ! credentials ?. password ) {
+        if ( ! credentials ?. username || ! credentials ?. password ) {
           return null ;
         }
         const user = await prisma . user . findUnique ( {
           where : {
-            email : credentials . email ,
+            username : credentials . username ,
           } ,
         } ) ;
         if ( ! user || ! user . hashedPassword ) {
