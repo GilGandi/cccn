6798d205fc20c70862fc32615332ee88d8282c28
Giovani Raci Paganini <gilracipaganini@gmail.com>
2026-05-01 16:01:26 -0300

    revert

--- app/admin/providers.tsx
+++ app/admin/providers.tsx
@@ -1,12 +1,6 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
-import { Session } from 'next-auth' // Importe o tipo Session
 
-interface AdminProvidersProps {
-  children: React.ReactNode;
-  session: Session | null; // Adicione a prop session
-}
-
-export default function AdminProviders({ children, session }: AdminProvidersProps) {
-  return <SessionProvider session={session}>{children}</SessionProvider>
+export default function AdminProviders({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>
 }
