e300e0f766b4b08c687c3519868eb44247ec719b
Giovani Raci Paganini <gilracipaganini@gmail.com>
2026-05-01 13:09:10 -0300

    Add authentication options for NextAuth

---
+++ app/api/auth/options.ts
@@ -0,0 +1,56 @@
+// app/api/auth/options.ts (NOVO ARQUIVO)
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth"; // Importe o tipo NextAuthOptions
+
+const prisma = new PrismaClient();
+
+// EXPORTE O OBJETO authOptions AQUI
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          return null;
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/admin/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
